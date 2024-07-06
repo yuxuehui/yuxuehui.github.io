59ceadbc82d81108e4606efd6fd413e71753b0fa
yuxuehui <1170574199@qq.com>
2024-07-06 16:44:12 +0100

    1

--- site.config.js
+++ site.config.js
@@ -56,7 +56,7 @@ const CONFIG = {
   utterances: {
     enable: false,
     config: {
-      repo: process.env.NEXT_PUBLIC_UTTERANCES_REPO || "",
+      repo: "morethanmin/morethan-log",
       "issue-term": "og:title",
       label: "💬 Utterances",
     },
